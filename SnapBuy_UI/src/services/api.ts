ed6421adf4c9d1521b714ee8f99b141257871495
CODEWITHRISHU2005 <rg2822046@gmail.com>
2025-12-20 10:45:31 +0530

    modify @20-12-25

--- SnapBuy_UI/src/services/api.ts
+++ SnapBuy_UI/src/services/api.ts
@@ -39,7 +39,7 @@ const clearAuthTokens = () => {
   }
 };
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
