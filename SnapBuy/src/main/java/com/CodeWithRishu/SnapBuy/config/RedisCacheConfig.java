7b9401ba780340b9f737babaa5c79366a969366a
CODEWITHRISHU2005 <rg2822046@gmail.com>
2026-02-06 23:08:24 +0530

    update @06-02-26

--- SnapBuy/src/main/java/com/CodeWithRishu/SnapBuy/config/RedisCacheConfig.java
+++ SnapBuy/src/main/java/com/CodeWithRishu/SnapBuy/config/RedisCacheConfig.java
@@ -36,48 +36,6 @@ import java.util.Optional;
 @EnableCaching
 public class RedisCacheConfig {
 
-    @Value("${spring.data.redis.url}")
-    private String redisUrl;
-
-    @Bean
-    @Primary
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return Optional.ofNullable(redisUrl)
-                .map(URI::create)
-                .map(uri -> {
-                    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-                    config.setHostName(uri.getHost());
-                    config.setPort(uri.getPort() != -1 ? uri.getPort() : 6379);
-
-                    Optional.ofNullable(uri.getUserInfo())
-                            .filter(info -> !info.isEmpty())
-                            .map(info -> info.contains(":") ? info.split(":", 2)[1] : info)
-                            .ifPresent(config::setPassword);
-
-                    LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigBuilder =
-                            LettuceClientConfiguration.builder()
-                                    .clientOptions(ClientOptions.builder()
-                                            .socketOptions(SocketOptions.builder()
-                                                    .connectTimeout(Duration.ofSeconds(30))
-                                                    .keepAlive(true)
-                                                    .build())
-                                            .autoReconnect(true)
-                                            .build())
-                                    .commandTimeout(Duration.ofSeconds(30));
-
-                    if ("rediss".equals(uri.getScheme())) {
-                        clientConfigBuilder.useSsl().disablePeerVerification();
-                    }
-
-                    LettuceClientConfiguration clientConfig = clientConfigBuilder.build();
-
-                    LettuceConnectionFactory factory = new LettuceConnectionFactory(config, clientConfig);
-                    factory.afterPropertiesSet();
-                    return factory;
-                })
-                .orElseThrow(() -> new IllegalStateException("Redis URL cannot be null"));
-    }
-
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
