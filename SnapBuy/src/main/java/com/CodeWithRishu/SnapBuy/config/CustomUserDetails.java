0fb2b16a22f5013823b537e5738a0099b0569019
CODEWITHRISHU2005 <rg2822046@gmail.com>
2026-06-06 11:18:01 +0530

    update @06-06-26

--- SnapBuy/src/main/java/com/CodeWithRishu/SnapBuy/config/CustomUserDetails.java
+++ SnapBuy/src/main/java/com/CodeWithRishu/SnapBuy/config/CustomUserDetails.java
@@ -2,6 +2,7 @@ package com.CodeWithRishu.SnapBuy.config;
 
 import com.CodeWithRishu.SnapBuy.entity.User;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
